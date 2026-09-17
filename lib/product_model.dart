decd657f13c4b7ef19f97420d7420217357225f4
yycapalot <tamtam5265123@gmail.com>
2026-09-17 22:24:32 +0800

    Add http package and products models with safe JSON parsing

---
+++ lib/product_model.dart
@@ -0,0 +1,62 @@
+// Product details
+class Product{
+  final int id;
+  final String title;
+  final String description;
+  final String category;
+  final double price;
+  final double rating;
+  final String thumbnail;
+  final List<String> images;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.category,
+    required this.description,
+    required this.price,
+    required this.rating,
+    required this.thumbnail,
+    required this.images
+});
+
+  factory Product.fromJson(Map<String, dynamic> json){
+    return Product(
+      id: json['id'] ?? 0,
+      title: json['title'] ?? '',
+      category: json['category'] ?? '',
+      description: json['description'] ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0.0,
+      rating: (json['rating'] as num?)?.toDouble() ?? 0.0,
+      thumbnail: json['thumbnail'] ?? '',
+      images: List<String>.from(json['images'] ?? []),
+    );
+  }
+}
+
+// Product pagination details
+class ProductResponse{
+  final List<Product> products;
+  final int total;
+  final int skip;
+  final int limit;
+
+  ProductResponse({
+    required this.products,
+    required this.total,
+    required this.skip,
+    required this.limit,
+});
+
+  factory ProductResponse.fromJson(Map<String, dynamic> json){
+    return ProductResponse(
+     products: (json['products'] as List?)
+         ?.map((item) => Product.fromJson(item))
+         .toList() ??
+        [],
+      total: json['total'] ?? 0,
+      skip: json['skip'] ?? 0,
+      limit: json['limit'] ?? 0,
+    );
+  }
+}
